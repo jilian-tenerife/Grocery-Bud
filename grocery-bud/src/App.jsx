906728d5be345d3c6f7eac5c9c65176d91c10753
Jillian <jillian.tenerife@usjr.edu.ph>
2022-11-11 05:10:13 +0800

    Alert

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -3,47 +3,52 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-  const [name, setName] = useState("")
-  const [list, setList] = useState([])
-  const [isEditing, setIsEditing] = useState(false)
-  const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" })
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!name) {
+    if (!name) {
       //display alert
-    }
-    else if (name && isEditing){
-
-    }
-    else{
-      const newItem = {id: new Date().getTime().toString(), title:name}
+      showAlert(true, "danger", "Please enter a value");
+    } else if (name && isEditing) {
+    } else {
+      const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
-      setName('')
+      setName("");
     }
+  };
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg })
   }
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removAlert={showAlert} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
-          <input type="text" className="grocery" placeholder="e.g eggs" value={name} onChange={(e)=>setName(e.target.value)}/>
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <button type="submit" className="submit-btn">
             {isEditing ? "edit" : "submit"}
           </button>
         </div>
       </form>
-      {list.length > 0 &&(
+      {list.length > 0 && (
         <div className="grocery-container">
-        <List items = {list}/>
-        <button className="clr-btn">Clear items</button>
-      </div>
+          <List items={list} />
+          <button className="clr-btn">Clear items</button>
+        </div>
       )}
-      
     </section>
   )
 }
-export default App
-
+export default App;
